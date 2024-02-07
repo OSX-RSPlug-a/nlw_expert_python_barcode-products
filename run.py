8be6813d9b807eeab8e28bb739c4fd35d804a2eb
OSX-RSPlug-a <log-n@outlook.com>
2024-02-07 00:54:59 -0300

    Adding structure

---
+++ run.py
@@ -0,0 +1,5 @@
+from src.main.server.server import app
+
+if __name__ == '__main__':
+    app.run(host=host, port=3333, debug=True)
+    
--- run_raw.py
+++ run_raw.py
@@ -3,8 +3,6 @@ from barcode import Code128
 from barcode.writer import ImageWriter
 
 
-app = Flask(__name__)
-
 @app.route('/create_tag', methods=["POST"])
 def create_tag():
     body = request.json
